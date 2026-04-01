dce5608bcb737b24cf8d0201acc6be1a0d7d4fc2
jcarl <j.carloss82@hotmail.com>
2026-04-02 01:48:43 +0200

    Fix gitignore: excluir .svelte-kit

--- .svelte-kit/output/client/_app/immutable/chunks/Cbua2gtn.js
+++
@@ -1,1 +0,0 @@
-import"./Vqgel5pW.js";
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.CmKUEsJU.js","_app/immutable/chunks/Vqgel5pW.js","_app/immutable/chunks/0Weyu5-e.js","_app/immutable/chunks/Cbua2gtn.js","_app/immutable/chunks/DXLwiZ0H.js"];
-export const stylesheets = [];
-export const fonts = [];
